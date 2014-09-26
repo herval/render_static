f4eb3d46dcd8b0b3872f0d0dfdfa622b120261e7
Kumar <arunkumar@chronus.com>
2014-09-26 22:13:36 +0530

    Replace selenium with phantomjs

--- lib/render_static/renderer.rb
+++ lib/render_static/renderer.rb
@@ -1,16 +1,13 @@
 require 'selenium-webdriver'
-require 'headless'
+require 'phantomjs'
 
 module RenderStatic
   class Renderer
 
     def self.render(env)
-      Headless.ly do
-        browser = Selenium::WebDriver.for(:firefox)
-        path = "#{env["rack.url_scheme"]}://#{env["HTTP_HOST"]}#{env["REQUEST_PATH"]}"
-        browser.navigate.to(path)
-        [200, { "Content-Type" => "text/html" }, [browser.page_source]] # TODO status code not supported by selenium
-      end
+      path = "#{env["rack.url_scheme"]}://#{env["HTTP_HOST"]}#{env["REQUEST_PATH"]}"
+      byebug
+      Phantomjs.run('./phantomjs/phantomjs-runner.js', path)
     end
   end
 end
